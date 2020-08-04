061f53caee56020a335765fed34e2990922aac14
unknown <rakhafikran@gmail.com>
2020-08-04 12:28:40 +0700

    Updates

---
+++ precache-manifest.8f88f408f5d9f6c2ffcdf609d50ae70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a372b3cb6c78bed81bd11501ff3ffa",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "c37c78d997af5dcb2bee",
+    "url": "/monsters-roledex/static/css/main.67b62d92.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-roledex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37c78d997af5dcb2bee",
+    "url": "/monsters-roledex/static/js/main.531fe843.chunk.js"
+  },
+  {
+    "revision": "5ef3fd6216758aff60f7",
+    "url": "/monsters-roledex/static/js/runtime-main.6db52b20.js"
+  }
+]);
